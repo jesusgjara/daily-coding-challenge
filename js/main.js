223ba7238eec8630170ad3bd0f284a96bcc95848
Jesus G Jara <jesusgjara.1@gmail.com>
2022-04-28 13:42:50 -0400

    codewars Counting sheep...

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ function positiveSum(arr) {
     return positive.reduce((acc, c) => acc + c, 0) 
   }return val
 }
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// For example,
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+//Hint: Don't forget to check for bad values like null/undefined
+
+function countSheeps(arrayOfSheep) {
+    let sheep = arrayOfSheep.filter(elem => elem === true)
+    return sheep.reduce((acc, c) => acc + c, 0) 
+}
