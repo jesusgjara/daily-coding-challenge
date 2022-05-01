b9ded6bb93ba6fa3e3a820b206a7c1f61500c53f
Jesus G Jara <jesusgjara.1@gmail.com>
2022-05-01 00:11:30 -0400

    codewars Jenny's secret message

--- js/main.js
+++ js/main.js
@@ -63,3 +63,12 @@ function lovefunc(flower1, flower2){
   } return false
 }
 
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+// Can you help her?
+
+function greet(name){
+  if(name === "Johnny") {
+    return "Hello, my love!";
+  } return "Hello, " + name + "!";
+}
