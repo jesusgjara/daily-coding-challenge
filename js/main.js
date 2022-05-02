825dab3d11ec7153a0aa55d54096fddcfa12e122
Jesus G Jara <jesusgjara.1@gmail.com>
2022-05-01 22:48:46 -0400

    codewars Grasshopper - Summation

--- js/main.js
+++ js/main.js
@@ -81,3 +81,18 @@ var stringToNumber = function(str){
  
   return Number(str);
 }
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. i.e:
+// summation(2) -> 3
+// 1 + 2
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+var summation = function (num) {
+  let result = 0;
+  for (let i = 1; i <= num; i++) {
+    result += i;
+  }
+  
+  return result;
+}
