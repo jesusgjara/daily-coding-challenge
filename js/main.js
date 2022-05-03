6fdfd2f4ce776dd91279ccef4d159a7f2440585f
Jesus G Jara <jesusgjara.1@gmail.com>
2022-05-02 23:30:25 -0400

    codewars You Can't Code Under Pressure #1

--- js/main.js
+++ js/main.js
@@ -96,3 +96,10 @@ var summation = function (num) {
   
   return result;
 }
+
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+  return i*2;
+}
+
