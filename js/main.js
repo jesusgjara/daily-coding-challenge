249b5aa8d12484efeb15a7d759a12ba97646d91e
Jesus G Jara <jesusgjara.1@gmail.com>
2022-05-02 23:33:14 -0400

    codewars Convert boolean values to strings 'Yes' or 'No'.

--- js/main.js
+++ js/main.js
@@ -103,3 +103,10 @@ function doubleInteger(i) {
   return i*2;
 }
 
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  if ( bool === true ) {
+      return ( "Yes" ); } 
+      return ( "No" ); 
+}
