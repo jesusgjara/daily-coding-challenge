5a6a08a9355190ad1d3b63d92a56aa1ad30458a6
Jesus G Jara <jesusgjara.1@gmail.com>
2022-05-04 22:31:26 -0400

    codewars Beginner Series #3 Sum of Numbers

--- js/main.js
+++ js/main.js
@@ -151,3 +151,24 @@ console.log(litres(3),'-->',1)
 console.log(litres(7),'-->',3)
 console.log(litres(6.7),'-->',3)
 console.log(litres(11.8),'-->',5)
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+// Note: a and b are not ordered!
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+function getSum( a,b ) {
+  let start = a < b ? a : b;
+  let end = a > b ? a : b;
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
