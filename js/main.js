7c8bac523f0022aa205d97b605acf97c6a98a455
Jesus G Jara <jesusgjara.1@gmail.com>
2022-05-09 19:02:17 -0400

    codewars Convert number to reversed array of digits

--- js/main.js
+++ js/main.js
@@ -210,3 +210,17 @@ function noSpace(x){
 // Note: Only valid inputs will be given.
 
 const booleanToString = b => b === true ? 'true': 'false';
+
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+// Example:
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
+
+const digitize = n => {
+  let str = n.toString(); 
+  let arr = [];
+  for(let i= str.length-1; i >= 0; i--) {
+    arr.push(Number(str[i]));
+  }
+  return arr
+}
