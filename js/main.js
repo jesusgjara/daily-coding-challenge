c9bdc3289ea4ca073fafd9d630fc818f772c6685
Jesus G Jara <jesusgjara.1@gmail.com>
2022-05-11 00:00:16 -0400

    codewars Parse nice int from char problem

--- js/main.js
+++ js/main.js
@@ -254,3 +254,12 @@ function abbrevName(name){
   let str = `${arr[0]}.${arr[index+1]}`
   return str.toUpperCase();
 }
+
+// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+// Write a program that returns the girl's age (0-9) as an integer.
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+function getAge(inputString){
+  let x = inputString[0];
+  return eval(x)
+}
