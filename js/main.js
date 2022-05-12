cce9c35b1dfa6e0cf7445fd5faf20d4273ea0484
Jesus G Jara <jesusgjara.1@gmail.com>
2022-05-11 22:05:05 -0400

    codewars Square Every Digit

--- js/main.js
+++ js/main.js
@@ -263,3 +263,16 @@ function getAge(inputString){
   let x = inputString[0];
   return eval(x)
 }
+
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+// Note: The function accepts an integer and returns an integer
+
+function squareDigits(num){
+  let string = num.toString();
+  let results = [];
+  for (var i = 0; i < string.length; i++){
+      results[i] = string[i] * string[i];
+  }
+  return Number(results.join(''));
+};
