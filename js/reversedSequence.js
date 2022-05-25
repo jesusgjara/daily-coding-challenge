f7af9551b7d71ca4ed6b2cbf32299741cd77bd8d
Jesus G Jara <jesusgjara.1@gmail.com>
2022-05-25 12:12:38 -0400

    Refactor Reversed Sequence

--- js/reversedSequence.js
+++ js/reversedSequence.js
@@ -2,12 +2,21 @@
 
 // Example : n=5 --> [5,4,3,2,1]
 
-const reverseSeq = n => {
-    let arr=[]
-    for (let i=1;i<=n;i++) {
-      if (n>0) {
-        arr.push(i)
-      }
-    }
-    return arr.reverse();
-  };
+// const reverseSeq = n => {
+//     let arr=[]
+//     for (let i=1;i<=n;i++) {
+//       if (n>0) {
+//         arr.push(i)
+//       }
+//     }
+//     return arr.reverse();
+//   };
+
+  //Better logic for loop
+
+  const reverseSeq = n => {
+    let arr = [];
+      for (let i=n; i>0; i--) {
+        arr.push(i);
+        } return arr;
+    };
