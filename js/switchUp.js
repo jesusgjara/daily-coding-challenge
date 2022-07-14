bdbac305169fb9c4931c8269e7781ae73e655455
Jesus G Jara <jesusgjara.1@gmail.com>
2022-07-13 22:00:09 -0400

    Switch it Up (alternative answer)

--- js/switchUp.js
+++ js/switchUp.js
@@ -4,36 +4,40 @@
 
 // Output :: "One".
 
-function switchItUp(number){
-    switch(number) {
-      case 1:
-      return 'One'
-      break;
-      case 2:
-      return 'Two'
-      break;
-      case 3:
-      return 'Three'
-      break;
-      case 4:
-      return 'Four'
-      break;
-      case 5:
-      return 'Five'
-      break;
-      case 6:
-      return 'Six'
-      break;
-      case 7:
-      return 'Seven'
-      break;
-      case 8:
-      return 'Eight'
-      break;
-      case 9:
-      return 'Nine'
-      break;
-      default:
-      return 'Zero'
-    }
-  }
+// function switchItUp(number){
+//     switch(number) {
+//       case 1:
+//       return 'One'
+//       break;
+//       case 2:
+//       return 'Two'
+//       break;
+//       case 3:
+//       return 'Three'
+//       break;
+//       case 4:
+//       return 'Four'
+//       break;
+//       case 5:
+//       return 'Five'
+//       break;
+//       case 6:
+//       return 'Six'
+//       break;
+//       case 7:
+//       return 'Seven'
+//       break;
+//       case 8:
+//       return 'Eight'
+//       break;
+//       case 9:
+//       return 'Nine'
+//       break;
+//       default:
+//       return 'Zero'
+//     }
+//   }
+
+//alternative answer
+
+const switchItUp = n => ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][n];
