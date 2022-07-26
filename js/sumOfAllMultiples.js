3e99a9d4b40bd4954b5a8cd92ae0163ed1fa82c8
Jesus G Jara <jesusgjara.1@gmail.com>
2022-07-26 00:29:11 -0400

    Refactor sumOfAllMultiples

--- js/sumOfAllMultiples.js
+++ js/sumOfAllMultiples.js
@@ -8,21 +8,21 @@
 
 // findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
 
-// function findSum(n) {
-//   let result = [];
-//   for(let i = 0; i <= n; i++) {
-//     if( i % 3 === 0 || i % 5 === 0){
-//       result.push(i)
-//     }
-//   }
-//   return result.reduce(( a, b ) => a + b)
-// } 
 function findSum(n) {
-    let result = 0;
-    for(let i = 0; i <= n; i++) {
-      if( i % 3 === 0 || i % 5 === 0){
-        result += i
-      }
+  let result = [];
+  for(let i = 0; i <= n; i++) {
+    if( i % 3 === 0 || i % 5 === 0){
+      result.push(i)
     }
-    return result
-}
+  }
+  return result.reduce(( a, b ) => a + b)
+} 
+// function findSum(n) {
+//     let result = 0;
+//     for(let i = 0; i <= n; i++) {
+//       if( i % 3 === 0 || i % 5 === 0){
+//         result += i
+//       }
+//     }
+//     return result
+// }
