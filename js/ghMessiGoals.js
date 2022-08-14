33e099141905cc21163394d93af63770a50a08dd
Jesus G Jara <jesusgjara.1@gmail.com>
2022-08-13 21:20:43 -0400

    GH: Messi Goals

---
+++ js/ghMessiGoals.js
@@ -0,0 +1,22 @@
+// Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+const laLigaGoals = 43
+const championsLeagueGoals = 10
+const copaDelReyGoals = 5
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
