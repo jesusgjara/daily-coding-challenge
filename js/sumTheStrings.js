8fc891e62a5b514e1ab7c017ff2cc5d730521a6d
Jesus G Jara <jesusgjara.1@gmail.com>
2022-08-14 21:13:23 -0400

    Sum the strings

---
+++ js/sumTheStrings.js
@@ -0,0 +1,13 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+
+function sumStr(a,b) {
+    return (Number(a) + Number(b)).toString()
+  }
