e53c536dbb36741ea005b73fcfcfed97eee27313
Jesus G Jara <jesusgjara.1@gmail.com>
2022-12-08 21:23:47 -0400

    Refactoring Messi Goals

--- js/ghMessiGoals.js
+++ js/ghMessiGoals.js
@@ -15,8 +15,39 @@
 // copaDelReyGoals
 // Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
 
-const laLigaGoals = 43
-const championsLeagueGoals = 10
-const copaDelReyGoals = 5
+// const laLigaGoals = 43
+// const championsLeagueGoals = 10
+// const copaDelReyGoals = 5
 
-const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+// const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+// Refactoring
+
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+
+const goalScoringStatistics = [
+  {
+    competition: 'La Liga',
+    goals: laLigaGoals,
+  },
+  {
+    competition: 'Champions League',
+    goals: championsLeagueGoals,
+  },
+  {
+    competition: 'Copa del Rey',
+    goals: copaDelReyGoals,
+  }
+]
+
+const countGoals = (arr) => {
+  let sum = 0;
+  arr.forEach((item) => {
+    sum += item.goals;
+  });
+  return sum;
+}
+
+let totalGoals = countGoals(goalScoringStatistics);
